e819ed91d5637861801db0c5470676dfa99eec0c
zhenyangze <zhenyangze@gmail.com>
2019-01-07 16:29:48 +0800

    add class

---
+++ UltiSnips/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Short description for index.php
+ *
+ * @package index
+ * @author zhenyangze <zhenyangze@gmail.com>
+ * @version 0.1
+ * @copyright (C) 2019 zhenyangze <zhenyangze@gmail.com>
+ * @license MIT
+ */
+
+$content  = file_get_contents('../Dict/layui.txt');
+$list = explode("\n", $content);
+foreach($list as $item) {
+    $item = trim($item);
+    if (empty($item)) {
+        continue;
+    }
+    $key = str_ireplace('-', '_', $item);
+    file_put_contents(__DIR__ . '/html_keywords.snippets', implode("\n", [
+        "snippet " . $key . ' ' . "\"Layui_Class:" . $item . "\"",
+        $item,
+        "endsnippet", 
+        "\n",
+    ]) . "\n", FILE_APPEND);
+}
